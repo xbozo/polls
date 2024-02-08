3369cf80fa3b262bb40cd6c1df3a5bc6a7b9312b
Guilherme Viana <imluizguilara@hotmail.com>
2024-02-07 23:57:27 -0300

    feat: setup prisma

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,25 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import z from 'zod'
 
 const app = fastify()
 
-app.get('/hello', () => {
-	return 'Hello NLW!'
+const prisma = new PrismaClient()
+
+app.post('/polls', async (req, rep) => {
+	const createPollBody = z.object({
+		title: z.string(),
+	})
+
+	const { title } = createPollBody.parse(req.body)
+
+	const poll = await prisma.poll.create({
+		data: {
+			title,
+		},
+	})
+
+	return rep.status(201).send({ pollId: poll.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
